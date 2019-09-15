2548aa66cb6da456352ac5d2ce7c92141ff660da
mrwavel <han@hanwavel.de>
2019-09-15 20:04:26 +0200

    Success in Test. Now starting the implementation

--- src/test/java/pdftester/PDFReaderTest.java
+++ src/test/java/pdftester/PDFReaderTest.java
@@ -2,9 +2,11 @@ package pdftester;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class PDFReaderTest {
 
@@ -13,8 +15,11 @@ public class PDFReaderTest {
      * Just a simple Read Test
      * @throws IOException
      */
-    public void testeRead() throws IOException {
-        PDDocument document = PDDocument.load(new File("test.pdf"));
+    @Test
+    public void testeRead() throws IOException, URISyntaxException {
+
+        File testFile = new File(this.getClass().getClassLoader().getResource("test.pdf").toURI());
+        PDDocument document = PDDocument.load(testFile);
         if (!document.isEncrypted()) {
             PDFTextStripper stripper = new PDFTextStripper();
             String text = stripper.getText(document);
